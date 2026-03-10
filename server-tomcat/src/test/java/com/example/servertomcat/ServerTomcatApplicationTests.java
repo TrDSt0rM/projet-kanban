daa6409bc61e7192d3165008f60f6f33e7dd1fb8
KoneNeil <koneneil@gmail.com>
2026-03-10 10:23:36 +0100

    init fichiers projets

---
+++ server-tomcat/src/test/java/com/example/servertomcat/ServerTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servertomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
