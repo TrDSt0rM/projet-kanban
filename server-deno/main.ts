74d8895582ec19fba15b0637b9ca0f72fa5d1503
Alex <alex.plociennik.ap@gmail.com>
2026-03-10 11:24:08 +0100

    refactor: Modification du fichier deno.json et main.ts feat: Ajout du fichier deno.lock
    - Modification du fichier deno.json pour ajouter les tâches de lancement du serveur
    - Modification du fichier main.ts pour ajouter le code principal du lancement du serveur
    - Ajout du fichier deno.lock qui contient les dépendances qui servent à lancer deno
    
    server-deno

--- server-deno/main.ts
+++ server-deno/main.ts
@@ -1,8 +1,46 @@
+import { Application, Router } from "@oak/oak";
+import { oakCors } from "@tajpouria/cors";
+
+// =========== Router Setup ==========
+
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello, World!";
+});
+
+// ========== Utility Functions ==========
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
-// @ts-ignore
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 }
+
+// =========== Main Application Code ==========
+
+const PROTOCOL = "http";
+const HOSTNAME = "localhost";
+const PORT = 8000;
+const ADDRESS = `${PROTOCOL}://${HOSTNAME}:${PORT}`;
+
+const app = new Application();
+
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+  "listen",
+  () => console.log(`Server listening on ${ADDRESS}`),
+);
+
+if (import.meta.main) {
+  await app.listen({ hostname: HOSTNAME, port: PORT });
+}
+
+export { app }; 
+
+
