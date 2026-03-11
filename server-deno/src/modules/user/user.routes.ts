74b9fd98bdb360c67641b182aeceac28b5c1267b
Alex <alex.plociennik.ap@gmail.com>
2026-03-11 16:42:01 +0100

    feat: Ajout des fichiers d'authentification
    - Ajout du service d'authentification
    - Ajout des routes d'authentification
    - Documentation du fichier main.ts
    
    server-deno-authentication

---
+++ server-deno/src/modules/user/user.routes.ts
@@ -0,0 +1,7 @@
+/**
+ * @file server-deno/src/modules/user/user.routes.ts
+ * @description This file contains the routes for the user-related operations.
+ * @version 1.0.0
+ * @date 2026-03-11
+ */
+
