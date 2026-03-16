4eb325cf973379d308179df7260c4bb9f2e5dced
Alex <alex.plociennik.ap@gmail.com>
2026-03-16 11:19:14 +0100

    feat: Ajout du middleware d'authentification
    - Renommage de `auth.ts` en `auth.middleware.ts` pour une meilleure organisation
    - Ajout d'un middleware d'authentification pour sécuriser les routes nécessitant une authentification
    - Mise à jour des routes pour utiliser le nouveau middleware d'authentification

---
+++ server-deno/src/shared/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import { Context, Next, State } from "@oak/oak";
+import { verifyJWT } from "../utils/crypto.utils.ts";
+import { APIErrorCode, APIException } from "../types/mod.ts";
+import { LoginPayload } from "../../modules/auth/auth.types.ts";
+
+export interface AuthContext extends Context {
+  state: AuthState;
+}
+
+export interface AuthState extends State {
+  user?: LoginPayload;
+}
+
+export async function authMiddleware(ctx: AuthContext, next: Next) {
+  const authHeader = ctx.request.headers.get("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new APIException(APIErrorCode.UNAUTHORIZED, 401, "Token manquant ou mal formaté");
+  }
+
+  const token = authHeader.substring(7);
+  const payload = await verifyJWT(token);
+
+  if (!payload) {
+    throw new APIException(APIErrorCode.UNAUTHORIZED, 401, "Token invalide");
+  }
+
+  ctx.state.user = payload;
+
+  await next();
+}
+
+export async function adminOnlyMiddleware(ctx: AuthContext, next: Next) {
+  if (!(ctx.state.user?.role == "admin")) {
+    throw new APIException(APIErrorCode.UNAUTHORIZED, 403, "Accès admin requis");
+  }
+
+  await next();
+}
