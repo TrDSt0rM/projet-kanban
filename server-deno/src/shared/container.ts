862836eb4f725e53ebc05d3161a17876366e05a0
Alex <alex.plociennik.ap@gmail.com>
2026-03-16 12:12:37 +0100

    feat: Ajout de fonctionnalités de gestion
    - Ajout de l'instance de BoardService dans le container d'injection de dépendances
    - Ajout de l'url du serveur Tomcat dans le container d'injection de dépendances
    
    server-deno

--- server-deno/src/shared/container.ts
+++ server-deno/src/shared/container.ts
@@ -1,3 +1,8 @@
 import { AuthService } from "../modules/auth/auth.service.ts";
+import { BoardService } from "../modules/board/board.service.ts";
 
+// Initialisation des services
 export const authService = new AuthService();
+export const boardService = new BoardService();
+
+export const URL_SERVER_TOMCAT = "http://localhost:8080";
